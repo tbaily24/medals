71e7494bce1744c974f11f2ca9ad0e87f0689369
Trevor Baily <tbaily24@hotmail.com>
2019-06-20 12:38:54 -0500

    omg

--- test_decoder.js
+++ test_decoder.js
@@ -4,24 +4,8 @@ function hello () => {
 }
 
 var test = hello();
-<<<<<<< HEAD
 
 var testingRemote = "testing remote";
-<<<<<<< HEAD
-=======
-var 1 = 'A';
-var 2 = 'B';
-var 3 = 'C';
-var 4 = 'D';
-var 5 = 'E';
-var 6 = 'F';
-var 723 = "flasdlfk";
-
-var asdf = "asdf";
-
->>>>>>> origin
-=======
 
 
 var AnotherRemoteChange = "testing remote";
->>>>>>> f4e95b6bfd7d352d6d2c7ded9ba05e59f2b1af21
